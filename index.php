3687a9e842bdc78ec947c4fc53148c746a032a53
Zemepatis <justinas.sabaliauskas@hotmail.lt>
2017-06-24 10:54:33 +0300

    Update added php

--- index.php
+++ index.php
@@ -1,12 +1,21 @@
-<!DOCTYPE html>
-<html>
+<?php
+//requere "functions.php";
+//include "functions.php";
+ 
+ // external php source
+?>
+<?php
+        print_cells(200); 
+?>
 
+
+<!DOCTYPE html>
+
+<html>
 <head>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
     <link rel="stylesheet" type="text/css" href="css/jstyle.css">
-
-
 </head>
 
 <body>
@@ -18,17 +27,23 @@
                 <table class="table">
 
 
-                    <?php for ($x=1 ; $x <=4 00; $x++) { echo "<table><td> $x </td></table>"; } ?>
+                    <?php 
+                    function print_cells($count) {
+                        for ($x=1 ; $x <=$count; $x++) {
+                         echo '<td class="lenta">' . $x . '</td>';
+                        } 
+
+                    }//adding "\" -padeda apeiti ribojima
+                    ?>
 
 
                 </table>
                 <div class="col-md-4"></div>
 
-
-
-
             </div>
         </div>
+        
+        
 </body>
 
 </html>
