5072851014c5122b80521102395f9f017481db04
Zemepatis <justinas.sabaliauskas@hotmail.lt>
2017-06-24 09:55:22 +0300

    Formated looks of code

--- index.php
+++ index.php
@@ -1,35 +1,34 @@
 <!DOCTYPE html>
 <html>
-<head>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
-<link rel="stylesheet" type="text/css" href="css/jstyle.css">
-	
+<head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <link rel="stylesheet" type="text/css" href="css/jstyle.css">
+
 
 </head>
+
 <body>
-<div class="container">
-<div class="row">
-  <div class="col-md-4"></div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4"></div>
 
-  <div class="col-md-4">
-<table class="table">
-
-
-  <?php  
-for ($x = 1; $x <= 400; $x++) {
-  echo "<table><td> $x </td></table>";
-}
-?> 
+            <div class="col-md-4">
+                <table class="table">
 
 
-</table>
-  <div class="col-md-4"></div>
+                    <?php for ($x=1 ; $x <=4 00; $x++) { echo "<table><td> $x </td></table>"; } ?>
+
+
+                </table>
+                <div class="col-md-4"></div>
 
 
 
 
-</div>
-</div>
+            </div>
+        </div>
 </body>
+
 </html>
