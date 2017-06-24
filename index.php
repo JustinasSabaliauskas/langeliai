90aa14aad75ec2e8d21c787957e606ddde1fb023
JustinasSabaliauskas <JustinasSabaliauskas@users.noreply.github.com>
2017-06-24 08:38:24 +0300

    homework 2017.06.24

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<link rel="stylesheet" type="text/css" href="css/jstyle.css">
+	
+
+</head>
+<body>
+<div class="container">
+<div class="row">
+  <div class="col-md-4"></div>
+
+  <div class="col-md-4">
+
+  <?php  
+for ($x = 0; $x <= 100; $x++) {
+  echo "<table> <td> $x </td></table><br/> <table>";
+
+}
+?>  
+</tr>
+  <div class="col-md-4"></div>
+
+
+
+
+</div>
+</div>
+</body>
+</html>
