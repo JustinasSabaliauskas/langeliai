69d8f9a8ca4c2cecc5193e9c016d731767e8b8df
JustinasSabaliauskas <JustinasSabaliauskas@users.noreply.github.com>
2017-06-25 11:10:22 +0300

    Fixed syntax error

--- index.php
+++ index.php
@@ -1,18 +1,48 @@
-<?php 
-
-$t=1; 
-$karsta=( $t>= 30); 
-$silta = (15 <=$t); 
-$salta=( 5 <=$t); 
+<?php
 
 
-function get_feel() 
-{ if ($t>= 30) 
-    { echo "karsta"; } 
+function get_feel($t){
+if ($t <= 5) {
+    return "Salta!";
+} 
+if ($t >= 6) {
+    return "Silta!";
+} 
+else if ($t >= 29)
+{
+    return "Karsta!";
+}
+}
 
-else (15<=$t) 
-    { echo "silta"; }
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	
+</head>
+<body>
+<table>
+	
+	<tr>
+	<th> Temperatura</th> 
+	<th>Pojutis</th>
+	</tr>
+		<?php
+			for ($t=0; $t <=30 ; $t++) { 
+				echo "<tr><td>" .$t. "</td> <td>" .get_feel($t)."</td></tr>";
+			}
+			?>
+		
+</table>
 
-    ?>
+</body>
+</html>
+
+//namu darbam lentele su baseinu get_area() - turi baseini get volume.(3 paramentrai)
+//kiek reikes masinu uzpildyti baseina -get_truck  count
+// $turis = get_volume ($ilfis, $plotis, $gylis)
+//$sunkvezimiai = gauti sunkvezimius ($turi, $cisterna)
+
+//echo"jei ilgis yra" .$ilgis. "tai reikes" . 
 
         
