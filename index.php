c3d47a7be6fcf01648217c22a2035da5fb828345
JustinasSabaliauskas <JustinasSabaliauskas@users.noreply.github.com>
2017-06-26 15:40:16 +0300

    Beutifyed

--- index.php
+++ index.php
@@ -1,47 +1,48 @@
 <?php
-
-function get_area($turis)
-{
-	$gylis = rand(1, 50);
-
-	return $gylis;
-
-	$turis = 20 * 5 * $gylis;
-	
-	return $turis;
-}
-//function cisternos(){
-	//if ($turis <= 400 ) {
-		
-	//}
-	//}
-	//return $cisternos
-//}
-
-?>
-
+    function get_area($turis)
+    {
+    	$gylis = rand(1, 50);
+    
+    	return $gylis;
+    
+    	$turis = 20 * 5 * $gylis;
+    	
+    	return $turis;
+    }
+    //function cisternos(){
+    	//if ($turis <= 400 ) {
+    		
+    	//}
+    	//}
+    	//return $cisternos
+    //}
+    
+    ?>
 <!DOCTYPE html>
 <html>
-<head>
-<title>Baseinai ir autocisternos</title>
-
-</head>
-<body>
-<table>
-<tr>
-	<th>Baseino gylis </th> <th> Baseino turis</th> <th>Cisternos</th>
-	</tr>
-<?php
-
-
-		
-echo "<tr><td" .$gylis. "</td><td>" . get_area($turis). "</td></tr>";
-
-
-?>
-
-
-</table>
-
-</body>
+    <head>
+        <title>Baseinai ir autocisternos</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    </head>
+    <body>
+        <div class="conatiner">
+            <div class="row">
+                <div class="col-xs-6 col-md-4"></div>
+                <div class="col-xs-6 col-md-4">
+                    <table>
+                        <tr>
+                            <th>Baseino gylis </th>
+                            <th> Baseino turis</th>
+                            <th>Cisternos</th>
+                        </tr>
+                        <?php
+                            echo "<tr><td" .$gylis. "</td><td>" . get_area($turis). "</td></tr>";}
+                            
+                            ?>
+                    </table>
+                </div>
+                <div class="col-xs-6 col-md-4">.col-xs-6 .col-md-4</div>
+            </div>
+        </div>
+    </body>
 </html>
